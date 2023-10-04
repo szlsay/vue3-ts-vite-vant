1943c4e902cbf3209ee800ef76c77bbccc46a269
小沈曰 <409178030@qq.com>
2023-10-04 23:32:28 +0800

    api

--- vite-vue3-ts/vite.config.ts
+++ vite-vue3-ts/vite.config.ts
@@ -3,7 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],  
+  plugins: [vue()], 
+  server: {
+    port: 8002,
+    host: '0.0.0.0',
+    open: true,
+    proxy:{
+      '/api/upload':'https://mobile.zcwytd.com',
+      '/api':'https://api.imooc.zcwytd.com'
+    },
+    cors: true
+  },
   resolve: {
     alias:{
       '@': path.resolve(__dirname, './src')
