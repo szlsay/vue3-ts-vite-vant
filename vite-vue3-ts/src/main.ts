6838571c6182132805b88c0ff4f09f3945f431ac
小沈曰 <409178030@qq.com>
2023-10-04 23:36:29 +0800

    assets

--- vite-vue3-ts/src/main.ts
+++ vite-vue3-ts/src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import 'vant/lib/index.css'
-import './style.css'
+// import './style.css'
+import './assets/css/style.css'
 import App from './App.vue'
 import store from './store'
 import router from './router'
 import { Button,NavBar,Tabbar,TabbarItem,Checkbox,Toast,Icon,Popup,Swipe, SwipeItem,List,PullRefresh,ActionBarIcon,Search,
     Loading,Field,SwipeCell,Uploader,Form,ActionSheet,Picker,CheckboxGroup,Dialog,Switch,RadioGroup, Radio } from 'vant'
+import './utils/rem'
 const app = createApp(App)
 app.use(Button)
 app.use(NavBar)
